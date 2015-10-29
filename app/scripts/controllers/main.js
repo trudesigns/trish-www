b7f23bfe4cce26ff8ee1d1bf754fafbcbbe320c7
Love Kempe <love.kempe@gmail.com>
2015-10-29 22:57:40 +0100

    Code style.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,21 +1,33 @@
- 'use strict';
+(function () {
+   'use strict';
 
-/**
- * @ngdoc function
- * @name trishApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the trishApp
- */
-angular.module('trishApp')
-  .controller('MainCtrl', function ($scope, $document, $window, $timeout) {
+  /**
+   * @ngdoc function
+   * @name trishApp.controller:MainCtrl
+   * @description
+   * # MainCtrl
+   * Controller of the trishApp
+   */
+  angular
+    .module('trishApp')
+    .controller('MainCtrl', MainCtrl);
 
-    var ctrl = this;
+  MainCtrl.$inject = ['$scope', '$document', '$window', '$timeout'];
+
+  function MainCtrl ($scope, $document, $window, $timeout) {
+    var vm = this;
+
     var _currentAnchorOffset;
     var _previousAnchorOffset;
     var anchorElement = document.getElementById('anchorBeginIsScrolledDown');
     var navElement = document.getElementById('navbar');
-    // var $marginElement = angular.element(document.getElementById('extraMarginTop'));
+
+    vm.isCollapsed = true;
+    vm.isScrolledDown = null;
+    vm.toggleNav = toggleNav;
+    vm.scrollToTop = scrollToTop;
+
+    _init();
 
     function _init () {
       angular.element($window).bind('scroll', _onScroll);
@@ -32,17 +44,17 @@ angular.module('trishApp')
      * For when scrolling down past navbar while it's open in mobile view.
      */
     function _setAnchorMarginBottom (newValue) {
-      angular.element(anchorElement).css('margin-bottom', (newValue ? newValue : ctrl.isScrolledDown ? navElement.offsetHeight + 20 : 0) + 'px');
+      angular.element(anchorElement).css('margin-bottom', (newValue ? newValue : vm.isScrolledDown ? navElement.offsetHeight + 20 : 0) + 'px');
     }
 
     function _setIsScrolledDown () {
       _currentAnchorOffset = anchorElement.getBoundingClientRect().top;
       if (_currentAnchorOffset > 0) { // If scrolled above the splash-page
-        ctrl.isScrolledDown = false;
+        vm.isScrolledDown = false;
       } else { // Else is scrolled past splash-page
-        ctrl.isScrolledDown = true;
+        vm.isScrolledDown = true;
       }
-      if (ctrl.isScrolledDown && _currentAnchorOffset > _previousAnchorOffset) { // If scrolled down and scrolling downwards
+      if (vm.isScrolledDown && _currentAnchorOffset > _previousAnchorOffset) { // If scrolled down and scrolling downwards
         _setAnchorMarginBottom();
       } else {
         _setAnchorMarginBottom(0);
@@ -52,7 +64,7 @@ angular.module('trishApp')
 
     function toggleNav (collapse) {
       $timeout(_setAnchorMarginBottom, 10);
-      ctrl.isCollapsed = collapse || !ctrl.isCollapsed;
+      vm.isCollapsed = collapse || !vm.isCollapsed;
     }
 
     function scrollToTop () {
@@ -61,13 +73,5 @@ angular.module('trishApp')
       });
     }
 
-    angular.extend(ctrl, {
-      isCollapsed: true,
-      isScrolledDown: null,
-      toggleNav: toggleNav,
-      scrollToTop : scrollToTop
-    });
-
-    _init();
-
-  });
+  }
+})();
