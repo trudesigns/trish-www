c673f0a011139ee4baaf316a4ab7718172e85796
Tommy T Haegglund <sanghe108@gmail.com>
2015-12-05 15:48:17 -0800

    PUSH

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -29,6 +29,7 @@ module.exports = function(config) {
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/angular-scroll/angular-scroll.js',
+      'bower_components/bonzo/bonzo.js',
       // endbower
       "app/scripts/**/*.js",
       "test/mock/**/*.js",
